4b7dcbcf16329097723fca422cfb51d8e8a07265
deb <debashish.jagadeba@xinja.com.au>
2020-02-28 03:11:38 +1100

    initial push

---
+++ DebasisSanitaryware/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebasisSanitaryware.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Hindware()
+        {
+            return View();
+        }
+    }
+}
